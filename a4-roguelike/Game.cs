eabb030846e4edafc3e979a5426c444b28b98cdd
Eli-Mohawk <eli.poag@mohawkcollege.ca>
2025-11-07 13:24:11 -0500

    created project

---
+++ a4-roguelike/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace MohawkGame2D
+{
+    public class Game
+    {
+        public void Setup()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
