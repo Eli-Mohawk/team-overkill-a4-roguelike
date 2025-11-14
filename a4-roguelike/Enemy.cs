917aa544d4dbb0b9e2b5404b24a74380fa222528
Eli-Mohawk <eli.poag@mohawkcollege.ca>
2025-11-14 17:39:47 -0500

    add basic enemy-player stopping

--- a4-roguelike/Enemy.cs
+++ a4-roguelike/Enemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.InteropServices.Swift;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,16 +13,21 @@ namespace MohawkGame2D
 
         public Vector2 pos;
         public Vector2 size;
-        Vector2 speed = new Vector2(2, 2);
+        float speedNum;
+        Vector2 angle;
+        Vector2 speed;
+        Vector2 TEMP = new Vector2(30, 30);
 
-        public Enemy(Vector2 pos, Vector2 size)
+        public Enemy(Vector2 pos, Vector2 size, float speedNum)
         {
             this.pos = pos;
             this.size = size;
+            this.speedNum = speedNum;
         }
 
         public void Update(Player player, Enemy[] enemies)
         {
+            EnemyDistance(player, enemies);
             PlayerTracking(player, enemies);
             DrawEnemy();
         }
@@ -34,17 +40,46 @@ namespace MohawkGame2D
 
         void PlayerTracking(Player player, Enemy[] enemies)
         {
+            
+
             for (int i = 0; i < enemies.Length; i++)
             {
                 Enemy enemy = enemies[i];
 
-                enemy.pos = pos;
+                enemy.speed = new Vector2(enemy.speedNum, enemy.speedNum);
 
-                Vector2 distance = enemy.pos - player.pos;
+                enemy.angle = Vector2.Normalize(enemy.pos - player.pos);
+                enemy.pos -= enemy.angle * enemy.speed;
+            }
+        }
 
-                Vector2 angle = Vector2.Normalize(distance);
+        void EnemyDistance(Player player, Enemy[] enemies)
+        {
+            
 
-                pos -= angle;
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Enemy enemy = enemies[i];
+
+                float playerBoxLeft = player.pos.X - TEMP.X;
+                float playerBoxRight = player.pos.X + player.size.X + TEMP.X;
+                float playerBoxTop = player.pos.Y - TEMP.Y;
+                float playerBoxBottom = player.pos.Y + player.size.Y + TEMP.Y;
+
+                float enemyLeft = enemy.pos.X;
+                float enemyRight = enemy.pos.X + enemy.size.X;
+                float enemyTop = enemy.pos.Y;
+                float enemyBottom = enemy.pos.Y + enemy.size.Y;
+
+                bool isColliding = playerBoxLeft < enemyRight && playerBoxRight > enemyLeft && playerBoxTop < enemyBottom && playerBoxBottom > enemyTop;
+
+                if (isColliding)
+                {
+                    if (playerBoxLeft < enemyRight) enemy.pos.X = playerBoxLeft - enemy.size.X;
+                    else if (playerBoxRight > enemyLeft) enemy.pos.X = playerBoxRight;
+                    else if (playerBoxTop < enemyBottom) enemy.pos.Y = playerBoxTop - enemy.size.Y;
+                    else if (playerBoxBottom > enemyTop) enemy.pos.Y = playerBoxBottom;
+                }
             }
         }
     }
